f4e1d2013c9a1c64bfa6a6cd5999a85a7749dc70
Maciek <maciek.wajda@gmail.com>
2018-03-01 18:33:15 +0100

    Extract env variables.

--- src/main/java/config/DriverConfig.java
+++ src/main/java/config/DriverConfig.java
@@ -6,10 +6,12 @@ public class DriverConfig {
 
     private static final String DRIVERS_PATH = "build/webdrivers/";
     private static final String OS = "windows";
+    private static final String ARCH = "64bit";
+    private static final String EXT = ".exe";
 
     public static ChromeOptions chromeConfig(){
         System.setProperty("webdriver.chrome.driver", DRIVERS_PATH + OS
-                + "/googlechrome/64bit/chromedriver.exe");
+                + "/googlechrome/" + ARCH + "/chromedriver" + EXT);
         return new ChromeOptions();
     }
 }
